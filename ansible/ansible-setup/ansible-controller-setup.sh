51ea3474d453eee1612e5d6d0ac89c3f433638b0
root <root@ip-172-31-92-12.ec2.internal>
2022-06-13 15:25:21 +0000

    Adding ansible documentation

--- ansible/ansible-setup/ansible-controller-setup.sh
+++ ansible/ansible-setup/ansible-controller-setup.sh
@@ -19,21 +19,6 @@ su - ansible -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y"
 # Installing ansible
 amazon-linux-extras install ansible2 -y
 
-
-############################################
-  #setup node machine
-############################################
-useradd ansible
-echo "ansible" | passwd --stdin ansible
-
-# Providnig sudoers privilges
-echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# Enabling password based authentication
-sed -i s/PasswordAuthentication\ no// /etc/ssh/sshd_config
-sed -i s/\#PasswordAuthentication/PasswordAuthentication/ /etc/ssh/sshd_config
-systemctl reload sshd
-
 #####################################
       # Copy ssh key to target machine
 #####################################
